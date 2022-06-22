7e04c670d93ede8264b2cf548aa7f3ba46f47108
REGRAGUIahmed <ah.regragui@edu.umi.ac.ma>
2022-06-22 18:33:47 +0100

    Initial commit

---
+++ src/main/java/org/sid/appmvc/model/Bill.java
@@ -0,0 +1,17 @@
+package org.sid.appmvc.model;
+
+import lombok.Data;
+
+import java.util.Date;
+import java.util.List;
+
+@Data
+public class Bill {
+    private Long id;
+    private Date billingDate;
+    private Long customerID;
+    private Customer customer;
+    private List<Product> product;
+    private double prix;
+    private double qte;
+}
---
+++ src/main/java/org/sid/appmvc/model/Product.java
@@ -0,0 +1,12 @@
+package org.sid.appmvc.model;
+
+import lombok.Data;
+
+
+@Data
+public class Product {
+    private Long id;
+    private String designation;
+    private double prix;
+    private int quantite;
+}
